25564b912c8df3d9f4f69e9fefd9f53d69dd9d44
kishan <ramkishanvooka@gmail.com>
2020-06-27 14:43:19 +0530

    kishan

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Position from './Components/Positions';
 import Funds from './Components/Funds';
 import Profile from './Components/Profile';
 import OrderStatus from './Components/OrderStatus';
+import DashBoard from './Components/DashBoard'
 
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
@@ -18,18 +19,28 @@ class App extends Component {
 
   render () {
     return (    
-      <Router>
+      
         <div className="App">
-          <Navigation/>
-          <Route path= "/" exact component= {Orders} />
+          {/* <Navigation/> */}
+          <Router>
+            <Route path= "/" exact component= {DashBoard} />
+            <Route path= "/orders" component= {DashBoard}/>
+            <Route path= "/holdings" component={DashBoard} />
+            <Route path= "/positions" component={DashBoard} />
+            <Route path= "/funds" component= {DashBoard} />
+            <Route path= "/profile" component= {DashBoard} />
+            <Route path= "/orderstatus" component= {DashBoard} />
+          </Router>
+          
+          {/* <Route path= "/" exact component= {Orders} />
           <Route path= "/orders" component= {Orders}/>
           <Route path= "/holdings" component={Holdings} />
           <Route path= "/positions" component={Position} />
           <Route path= "/funds" component= {Funds} />
           <Route path= "/profile" component= {Profile} />
-          <Route path= "/orderstatus" component= {OrderStatus} />
+          <Route path= "/orderstatus" component= {OrderStatus} /> */}
         </div>
-      </Router>
+     
     )
   }
 }
